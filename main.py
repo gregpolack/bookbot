f3a4decbb673ae23de9fe5cd68be129b0aa1e20d
GregPolack <grzelock@gmail.com>
2024-07-07 19:59:33 +0200

    added functionality to count the number of characters

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-<<<<<<< HEAD
-def count_words(book):
-    words = book.split()
-    return len(words)
-
 def main():
+    
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         
         word_count = count_words(file_contents)
-        print(word_count)
-=======
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
->>>>>>> bf7d7bb9fd6c86c7fe242e303afc1eeb58593c90
+        char_count = count_chars(file_contents)
+
+        print(f"The number of words in the book is: {word_count}")
+        print("=" * 40)
+        print("Letter count inside the book:")
+        
+        for key in char_count:
+            print(f"{key} -> {char_count[key]}")
+
+def count_words(book):
+    
+    words = book.split()
+    return len(words)
+
+def count_chars(book):
+
+    char_count = {}
+    for letter in book:
+        if letter.isalpha():
+            letter = letter.lower()
+            if letter not in char_count:
+                char_count[letter] = 1
+            else:
+                char_count[letter] += 1
+    return char_count 
 
 if __name__ == "__main__":
     main()

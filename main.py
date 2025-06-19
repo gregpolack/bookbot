34e5d105ab2ac7624123f69af8ce76016a8754e4
gregpolack <grzelock@gmail.com>
2025-06-19 11:56:47 +0200

    update main.py to print list of characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_num_words
+from stats import count_num_words, count_num_chars, sort_count_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,8 +9,12 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_num_words(book_text)
+    num_chars = count_num_chars(book_text)
+
+    char_list = sort_count_chars(num_chars)
 
     print(f"{num_words} words found in the document")
+    print(char_list)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,3 +2,29 @@ def count_num_words(text):
     words = text.split()
     
     return len(words)
+
+def count_num_chars(text):
+    chars = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    
+    return chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_count_chars(chars):
+    char_dicts = []
+    for char in chars:
+        char_dict = {}
+        char_dict["char"] = char
+        char_dict["num"] = chars[char]
+        char_dicts.append(char_dict)
+    
+    char_dicts.sort(reverse=True, key=sort_on)
+    
+    return char_dicts

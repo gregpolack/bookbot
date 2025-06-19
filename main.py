e6dcc879c2f5cfd98ef0918d61c0be4cd7b1a1d4
gregpolack <grzelock@gmail.com>
2025-06-19 12:11:12 +0200

    update main.py to print book report

--- main.py
+++ main.py
@@ -7,14 +7,22 @@ def get_book_text(filepath):
     return book_text
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    filepath = "books/frankenstein.txt"
+
+    book_text = get_book_text(filepath)
     num_words = count_num_words(book_text)
     num_chars = count_num_chars(book_text)
-
     char_list = sort_count_chars(num_chars)
 
-    print(f"{num_words} words found in the document")
-    print(char_list)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print(f"--------- Character Count -------")
+    for char in char_list:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+    print ("============= END ===============")
 
 if __name__ == "__main__":
     main()

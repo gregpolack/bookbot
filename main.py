045542dd53939469ea3d66178820a3e2c49eaf19
gregpolack <grzelock@gmail.com>
2025-06-19 11:15:43 +0200

    update main.py to read text files

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        book_text = f.read()
+    
+    return book_text
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+if __name__ == "__main__":
+    main()

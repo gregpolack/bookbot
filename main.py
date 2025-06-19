f8ecb601d973f901ba584c7cf17c73b3840516f7
gregpolack <grzelock@gmail.com>
2025-06-19 11:29:21 +0200

    basic refactoring

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from stats import count_num_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         book_text = f.read()
     
     return book_text
-
-def count_num_words(text):
-    words = text.split()
-    
-    return len(words)
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
